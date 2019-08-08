9a806e05dfd4ffaee6fd97f8695185ba141c5d3b
Sunny <maroon96_@hotmail.com>
2019-08-08 17:01:26 +1000

    added fonts and fixed the margins/paddings, added image

--- luca-website/src/components/home-section.js
+++ luca-website/src/components/home-section.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './cssComponents/home-section.css';
+import luca from '../luca.jpeg'
 
 class HomeSection extends React.Component {
 
@@ -12,16 +13,17 @@ render () {
             <div class="home section ">
                 <div class="section-title-box-left">
                     <h6 class="section-title">The single source of truth for all transactions </h6>
-                    <div class="home-notes"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                    <div class="home-notes"> LucaPlus makes transactions simpler, faster and smarter, whilst ensuring the integrity of the data </div>
                     {/* <a class="button" href="#" role="button">Get started for free</a> */}
                     <button class="home-button">Get started for free</button>
                 </div>
                 <div class="section-content">
                     <h4>image inserted here</h4>
+                    <img src={luca} alt="luca-pic" className="Luca-pic" />
                 </div>
             </div>
 
-            <div class="home section ">
+            <div class="how-it-works section ">
                 <div class="section-title-box-left">
                     <h6 class="section-title">The single source of truth for all transactions </h6>
                     <div class="home-notes"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
@@ -30,6 +32,7 @@ render () {
                 </div>
                 <div class="section-content">
                     <h4>image inserted here</h4>
+                    <img src={luca} alt="luca-pic" className="Luca-pic" />
                 </div>
             </div>
 
