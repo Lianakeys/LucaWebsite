c5a24e12a26e4186ed965d19ee4107a2fcb14eba
Sunny <maroon96_@hotmail.com>
2019-08-09 15:16:40 +1000

    completed footer whole structure, fixed all flexboxes, done footernavbar, completed footer email form, added stylings for footer

--- luca-website/src/components/footer.js
+++ luca-website/src/components/footer.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./cssComponents/footer.css";
-
+import { Link } from "react-router-dom";
+import style from "./Navbar/Navbar.module.css";
+import LucaLOGO from './Luca-logo.svg'
 
 class Footer extends React.Component {
     render() {
@@ -24,12 +26,49 @@ class Footer extends React.Component {
                 </div>
                 <div className="footer-middle-grid">
                     <div className="footer-middle-links-container">
-                        hihi
+                        <ul className="footer-navbox">
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/Product">
+                                    Product
+                                </Link>
+                            </li>
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/Features">
+                                    Features
+                                </Link>
+                            </li>
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/Support">
+                                    Support
+                                </Link>
+                            </li>
+                           
+                            <div className="footer-logo-box">
+                                <img src={LucaLOGO} className="footer-luca-logo" alt="Luca Logo" />
+                                {/* <SVG /> */}
+                            </div>
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/About">
+                                    About
+                                </Link>
+                            </li>
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/Blog">
+                                    Blog
+                                </Link>
+                            </li>
+                            <li className="footer-navitem">
+                                <Link className={style.navlink} to="/ContactUs">
+                                    Contact us
+                                </Link>
+                            </li>
+                           
+                        </ul>
                     </div>
                 </div>
                 <div className="footer-bottom-grid">
                     <div className="copyright-terms-box">
-                        <p>© Copyright 2019 Blockledger | Terms of Service | Privacy & Security</p>
+                        <p className="copyright-font">© Copyright 2019 Blockledger | Terms of Service | Privacy & Security</p>
                     </div>
                 </div>
             </div>
