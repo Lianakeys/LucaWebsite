b0f0a5bab351ed793a99b7414edfa15710b479d8
Sunny <maroon96_@hotmail.com>
2019-08-12 09:15:06 +1000

    Updates

---
+++ precache-manifest.a6152e7d29b5810f066a21cbb9577472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbe93bdc5ac2279e37f5faf0d0e2973",
+    "url": "/LucaWebsite/index.html"
+  },
+  {
+    "revision": "8dc2f86df79439916681",
+    "url": "/LucaWebsite/static/css/main.17cfa2a7.chunk.css"
+  },
+  {
+    "revision": "ab1589ffb382bc7f06f8",
+    "url": "/LucaWebsite/static/js/2.75def320.chunk.js"
+  },
+  {
+    "revision": "8dc2f86df79439916681",
+    "url": "/LucaWebsite/static/js/main.e7f46c6a.chunk.js"
+  },
+  {
+    "revision": "b6a4c9bad745af943c7e",
+    "url": "/LucaWebsite/static/js/runtime~main.9ba4cac1.js"
+  },
+  {
+    "revision": "f814a3b13e0062d868f4bef33cbb1411",
+    "url": "/LucaWebsite/static/media/Hero-image.f814a3b1.svg"
+  },
+  {
+    "revision": "ec2df460e96046debb052642300f74d3",
+    "url": "/LucaWebsite/static/media/Luca-logo.ec2df460.svg"
+  }
+]);
