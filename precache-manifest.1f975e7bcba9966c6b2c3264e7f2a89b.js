9c3c8c844d4143f23ac9f236789b253312b0f2a1
Sunny <maroon96_@hotmail.com>
2019-08-12 14:49:19 +1000

    Updates

---
+++ precache-manifest.1f975e7bcba9966c6b2c3264e7f2a89b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc00b8f41fa1ed691b94bb5bfb02970",
+    "url": "/LucaWebsite/index.html"
+  },
+  {
+    "revision": "1bdf3e556906bd3a76ba",
+    "url": "/LucaWebsite/static/css/main.15fb67d3.chunk.css"
+  },
+  {
+    "revision": "52bd107ed19d5d2957da",
+    "url": "/LucaWebsite/static/js/2.60290e40.chunk.js"
+  },
+  {
+    "revision": "1bdf3e556906bd3a76ba",
+    "url": "/LucaWebsite/static/js/main.b1b94867.chunk.js"
+  },
+  {
+    "revision": "b6a4c9bad745af943c7e",
+    "url": "/LucaWebsite/static/js/runtime~main.9ba4cac1.js"
+  },
+  {
+    "revision": "f814a3b13e0062d868f4bef33cbb1411",
+    "url": "/LucaWebsite/static/media/Hero-image.f814a3b1.svg"
+  },
+  {
+    "revision": "ec2df460e96046debb052642300f74d3",
+    "url": "/LucaWebsite/static/media/Luca-logo.ec2df460.svg"
+  },
+  {
+    "revision": "4a65c257f48509bcdfebba7f6dd30436",
+    "url": "/LucaWebsite/static/media/firsticon.4a65c257.svg"
+  },
+  {
+    "revision": "db93306be4dd863fa968f781a0929de2",
+    "url": "/LucaWebsite/static/media/left-dash-line.db93306b.svg"
+  },
+  {
+    "revision": "a6a0069a107fecf233d51d975e112903",
+    "url": "/LucaWebsite/static/media/right-dash-line.a6a0069a.svg"
+  },
+  {
+    "revision": "83990b1acb14353455fd1c92517e5fdb",
+    "url": "/LucaWebsite/static/media/secondicon.83990b1a.svg"
+  },
+  {
+    "revision": "27c57a27ddf2585034b6cac4fecd78e1",
+    "url": "/LucaWebsite/static/media/thirdicon.27c57a27.svg"
+  }
+]);
