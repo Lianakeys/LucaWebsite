30e29515d5a16ea3ed82316d53a17bea2ab77c00
Sunny <maroon96_@hotmail.com>
2019-08-12 14:40:15 +1000

    create and completed how it works section

--- luca-website/src/App.js
+++ luca-website/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomeSection from "./components/home-section";
 import Footer from "./components/footer";
+import HowItWorks from "./components/how-it-works";
 // import Navbar from "./components/Navbar/Navbar";
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div className="App" >
         <meta charset="UTF-8"></meta>
         <HomeSection />
+        <HowItWorks />
         <Footer />
       </div>
     );
